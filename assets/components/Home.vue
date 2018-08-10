5813fc60abc2424e1cf311d17f120fdb401d97ae
chico1992 <cedric.correia.alves@gmail.com>
2018-08-10 20:35:33 +0200

    created first vue component

---
+++ assets/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+       <h1>This is an example of a new components in VueJs</h1>
+       <p>Bacon ipsum dolor amet tri-tip hamburger turducken venison alcatra shankle pancetta shank ball tip pork belly pork chop kevin beef. Flank prosciutto tail, pork shoulder chuck cupim frankfurter boudin porchetta bresaola. Kielbasa tail ham hock, burgdoggen bresaola sirloin fatback meatball shankle tongue doner turducken pork chop salami shank. Shank meatloaf prosciutto sirloin spare ribs, salami turkey bresaola kielbasa pastrami landjaeger shoulder hamburger t-bone alcatra. Fatback shank pancetta, prosciutto kevin tri-tip biltong burgdoggen picanha. Ball tip jowl venison shank salami rump buffalo brisket ham hock shoulder swine sausage beef. Drumstick hamburger jerky ball tip flank short loin swine ham chicken pork sausage buffalo shank.
+            Pork chop tri-tip bresaola, strip steak short ribs turducken meatball filet mignon sirloin pork. Pig ham sirloin short loin shank burgdoggen bacon beef ribs boudin rump alcatra prosciutto. Ham hock t-bone meatball pastrami ground round landjaeger kevin corned beef fatback alcatra pork chop salami turducken. Kevin fatback capicola buffalo burgdoggen andouille meatball. Ham hock salami turducken t-bone, filet mignon kevin shankle.
+            Chicken short loin ball tip, bacon frankfurter boudin ribeye fatback meatloaf leberkas. Turducken pastrami shankle beef chuck turkey alcatra cupim doner bacon meatloaf landjaeger chicken andouille jerky. Bacon flank shank, salami jowl pastrami burgdoggen frankfurter biltong kevin porchetta sausage. Bacon capicola pork ball tip pancetta tongue meatloaf. Short ribs leberkas sirloin turkey filet mignon.
+            Biltong boudin cow meatball swine porchetta pig. Sirloin doner picanha bacon, porchetta pork chop ground round tri-tip turkey corned beef pancetta strip steak pork. Pastrami kielbasa filet mignon, ham hock swine turkey pancetta ribeye. Doner cupim andouille brisket leberkas capicola porchetta chuck beef pastrami prosciutto kielbasa venison ribeye jowl. Bacon sirloin jerky, andouille bresaola jowl spare ribs prosciutto leberkas shoulder turkey swine frankfurter porchetta. Bresaola pastrami boudin ham hock andouille turkey.
+            Landjaeger pastrami corned beef fatback prosciutto ground round pork chop tenderloin meatball bacon pork belly jerky frankfurter. Andouille brisket sirloin, bresaola pig swine cupim bacon tail pork loin rump corned beef porchetta burgdoggen venison. Tongue venison capicola spare ribs porchetta. Pork loin pastrami frankfurter bresaola turkey venison capicola tenderloin t-bone short ribs burgdoggen pork chop bacon. Tri-tip cupim shoulder corned beef tail ball tip tenderloin bacon porchetta rump frankfurter. Ham hock doner shoulder, chicken shankle ball tip buffalo leberkas drumstick filet mignon alcatra rump. Ham hock kevin andouille porchetta sausage drumstick short ribs.</p>
+   </div>
+</template>
+ 
+<script>
+   export default {
+       name: "home"
+   }
+</script>
+ 
+<style scoped>
+ 
+</style>
---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// assets/js/app.js
+import Vue from 'vue';
+ 
+import Home from '../components/Home'
+ 
+/**
+* Create a fresh Vue Application instance
+*/
+new Vue({
+  el: '#app',
+  components: {Home}
+});
